fc2748f2b08e75130379e50b525da91b95feec19
Zse <zse@zse.pl>
2026-09-21 11:49:35 +0200

    konec lekcji

---
+++ Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace EventVsBindingMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            /*if (rotationSlider is not null)
+                rotationLabel.Rotation = rotationSlider.Value;*/
+
+            /*
+            Slider slider = sender as Slider;
+            if (slider is not null
+                && rotationLabel is not null)
+                rotationLabel.Rotation = slider.Value;
+
+            */
+
+            if (sender is Slider slider
+                && rotationLabel is not null)
+                rotationLabel.Rotation = slider.Value;
+        }
+
+    }
+}
---
+++ Programowanie/SimpleCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace SimpleCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //pobranie danych
+            //    pobranie pierwszej liczby
+            //    pobranie drugiej liczby
+            //obliczenie wyniku
+            //pokazanie wyniku
+
+            //int firstNumber = int.Parse(firstNumberEntry.Text);
+
+            //int firstNumber;
+            if (int.TryParse(firstNumberEntry.Text, out int firstNumber)
+                && int.TryParse(secondNumberEntry.Text, out int secondNumber))
+            {
+                int result = firstNumber + secondNumber;
+                resultLabel.Text = "Wynik to " + result;
+                resultLabel.BackgroundColor = Colors.Green;
+            }
+            else
+            {
+                resultLabel.Text = "Podałeś nieprawidłowe dane";
+                resultLabel.BackgroundColor = Colors.Red;
+            }
+        }
+    }
+}
